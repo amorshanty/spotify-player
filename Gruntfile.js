da683dd01ad44bd6d9542d43ec5373c931b477a5
Amor <amor.dibella@gmail.com>
2015-02-13 15:35:16 +0100

    added images and styles and content to index

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+      // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        mangle: false,
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      my_target: {
+        files: {
+          'build/main.min.js': ['javascript/*.js', 'bower_components/tinder-js-byamorshanty/*.js']
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+         'build/main.css':'sass/main.scss'
+        }
+      }
+    },
+    copy: {
+      main: {
+        src: 'bower_components/bootstrap/dist/css/bootstrap.min.css',
+        dest: 'build/bootstrap.min.css',
+      }
+    },
+    'gh-pages': {
+        options: {
+            base: 'build'
+        },
+        src: ['**']
+    }
+  });
+
+  // Load the plugin tasks
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  // Custom tasks
+  grunt.registerTask('deploy', ['gh-pages']);
+  grunt.registerTask('default', ['uglify', 'sass', 'copy', 'processhtml']);
+};
